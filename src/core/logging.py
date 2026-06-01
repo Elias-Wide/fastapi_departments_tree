12f0642129fe26e4fa636d6e25c3f42da15e7580
Ilya Shirokov <ilshi2904@gmail.com>
2026-06-01 15:03:10 +0300

    extend gitignore

--- src/core/logging.py
+++ src/core/logging.py
@@ -1,11 +1,18 @@
 import logging
 
 
-def get_logger(name: str) -> logging.Logger:
-    """Returns a configured logger instance for the given name."""
+def setup_logging() -> None:
+    """Configures the global logging settings for the application."""
     logging.basicConfig(
         level=logging.INFO,
-        format='%(filename)s:%(lineno)d #%(levelname)-8s '
-        '[%(asctime)s] - %(name)s - %(message)s',
+        format=(
+            "%(filename)s:%(lineno)d #%(levelname)-8s "
+            "[%(asctime)s] - %(name)s - %(message)s"
+        ),
+        force=True,
     )
+
+
+def get_logger(name: str) -> logging.Logger:
+    """Returns a configured logger instance for the given name."""
     return logging.getLogger(name)
--- src/main.py
+++ src/main.py
@@ -5,10 +5,14 @@ from fastapi import FastAPI
 from src.api.endpoints.v1.routers import api_router_v1 as main_router
 from src.core.exceptions.handler import service_error_handler
 from src.core.exceptions.services.base import ServiceError
+from src.core.logging import get_logger, setup_logging
+
+logger = get_logger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    setup_logging() 
     yield
 
 
